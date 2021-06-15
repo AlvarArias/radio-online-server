349012ced8711eb7f66954823a5e4d37965052ee
Alvar Arias <AlvarArias@users.noreply.github.com>
2021-06-15 13:38:35 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
 //const port = 3000
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('my-radio'))
 
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`)
 })
